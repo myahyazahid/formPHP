93b610db487796f251b2b4801db012169b1b45f8
Muhammad Yahya Zahid <myahyazahid11@gmail.com>
2026-03-21 22:26:58 +0700

    fetch database

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
+
+
+     <?php 
+    include 'config.php';
+    $tes = "Yahya";
+    $query = "select * from mahasiswa";
+    $result = mysqli_query($koneksi, $query);
+
+
+
+    // while($data = mysqli_fetch_assoc($result)){
+
+    // echo $data['nim'] . $data['nama'];
+
+    // };
+
+    $data = []; 
+
+    while ($row = mysqli_fetch_assoc($result)) {
+    $data[] = $row;
+}
+
+
+
+    ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -30,24 +57,27 @@
            
         </div>
 
+
         <!-- User Table -->
         <div class="overflow-x-auto bg-white rounded-lg shadow">
             <table class="w-full table-auto">
                 <thead>
                     <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-                        <th class="py-3 px-6 text-left">ID</th>
-                        <th class="py-3 px-6 text-left">Name</th>
-                        <th class="py-3 px-6 text-left">Email</th>
-                        <th class="py-3 px-6 text-left">Role</th>
+                        <th class="py-3 px-6 text-left">No</th>
+                        <th class="py-3 px-6 text-left">Nama</th>
+                        <th class="py-3 px-6 text-left">Nim</th>
+                        <th class="py-3 px-6 text-left">Jurusan</th>
                         <th class="py-3 px-6 text-center">Actions</th>
                     </tr>
                 </thead>
+
+                <?php foreach ($data as $mhs): ?>
                 <tbody class="text-gray-600 text-sm">
                     <tr class="border-b border-gray-200 hover:bg-gray-100">
                         <td class="py-3 px-6 text-left">1</td>
-                        <td class="py-3 px-6 text-left">Abhiraj k</td>
-                        <td class="py-3 px-6 text-left">abhi@kerala.com</td>
-                        <td class="py-3 px-6 text-left">Admin</td>
+                        <td class="py-3 px-6 text-left"><?= $mhs['nama']; ?></td>
+                        <td class="py-3 px-6 text-left"><?= $mhs['nim']; ?></td>
+                        <td class="py-3 px-6 text-left"><?= $mhs['jurusan']; ?></td>
                         <td class="py-3 px-6 text-center">
                             <div class="flex item-center justify-center">
                                 <button class="w-4 mr-2 transform hover:text-blue-500 hover:scale-110">
@@ -63,89 +93,14 @@
                             </div>
                         </td>
                     </tr>
-                    <tr class="border-b border-gray-200 hover:bg-gray-100">
-                        <td class="py-3 px-6 text-left">2</td>
-                        <td class="py-3 px-6 text-left">Ayyam Perumal</td>
-                        <td class="py-3 px-6 text-left">ayyam@kerala.com</td>
-                        <td class="py-3 px-6 text-left">Manager</td>
-                        <td class="py-3 px-6 text-center">
-                            <div class="flex item-center justify-center">
-                                <button class="w-4 mr-2 transform hover:text-blue-500 hover:scale-110">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                                    </svg>
-                                </button>
-                                <button class="w-4 mr-2 transform hover:text-red-500 hover:scale-110">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                    </svg>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr class="border-b border-gray-200 hover:bg-gray-100">
-                        <td class="py-3 px-6 text-left">3</td>
-                        <td class="py-3 px-6 text-left">George Thomas</td>
-                        <td class="py-3 px-6 text-left">george@kerala.com</td>
-                        <td class="py-3 px-6 text-left">User</td>
-                        <td class="py-3 px-6 text-center">
-                            <div class="flex item-center justify-center">
-                                <button class="w-4 mr-2 transform hover:text-blue-500 hover:scale-110">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                                    </svg>
-                                </button>
-                                <button class="w-4 mr-2 transform hover:text-red-500 hover:scale-110">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                    </svg>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr class="border-b border-gray-200 hover:bg-gray-100">
-                        <td class="py-3 px-6 text-left">4</td>
-                        <td class="py-3 px-6 text-left">Vasudev Menon</td>
-                        <td class="py-3 px-6 text-left">vasudev@kerala.com</td>
-                        <td class="py-3 px-6 text-left">Admin</td>
-                        <td class="py-3 px-6 text-center">
-                            <div class="flex item-center justify-center">
-                                <button class="w-4 mr-2 transform hover:text-blue-500 hover:scale-110">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                                    </svg>
-                                </button>
-                                <button class="w-4 mr-2 transform hover:text-red-500 hover:scale-110">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                    </svg>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr class="border-b border-gray-200 hover:bg-gray-100">
-                        <td class="py-3 px-6 text-left">5</td>
-                        <td class="py-3 px-6 text-left">Mandan Pillai</td>
-                        <td class="py-3 px-6 text-left">mandan@kerala.com</td>
-                        <td class="py-3 px-6 text-left">User</td>
-                        <td class="py-3 px-6 text-center">
-                            <div class="flex item-center justify-center">
-                                <button class="w-4 mr-2 transform hover:text-blue-500 hover:scale-110">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                                    </svg>
-                                </button>
-                                <button class="w-4 mr-2 transform hover:text-red-500 hover:scale-110">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                    </svg>
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
+
+                    <?php endforeach; ?>
+                
                 </tbody>
             </table>
         </div>
+
+        
 
         <!-- Static Pagination -->
         <div class="flex justify-between items-center mt-6">
